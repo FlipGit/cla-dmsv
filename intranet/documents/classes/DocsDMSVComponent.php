f3db9dc1c5106afb15be23da51a7afb280f8dc0a
Julian Cohen <julian@juco.co.uk>
2013-04-10 12:17:30 +0100

    Some commenting

--- intranet/documents/classes/DocsDMSVComponent.php
+++ intranet/documents/classes/DocsDMSVComponent.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * DMSV Component
+ *
+ * To use this, simply embed:
+ *
+ *	 <component class="DocsDMSVComponent">
+ *
+ * On your page.
+ *
+ * Optional parameters:
+ *  * root_folder - The folder to point the DMSV at
+ *
+ * @author Julian Cohen [julian.cohen@claromentis.com]
+ */
 class DocsDMSVComponent extends TemplaterComponentTmpl {
 
 	public function Show($attributes)
@@ -17,6 +31,7 @@ class DocsDMSVComponent extends TemplaterComponentTmpl {
 		// Check for a root folder
 		$args['root_folder.body'] = is_numeric($attributes['root_folder']) ? $attributes['root_folder'] : 0;
 
+		// Pass along some options
 		$args['dmsv_options.json'] = array('root_edit' => ERMSBrowser::GetPermsChecker(false)->CanChangeContent(null));
 
 		return $this->CallTemplater('documents/dms_view.html', $args, $datasources);
