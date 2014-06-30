b9b566c18147dde53330abd6a0efe47860444f5c
d.kortelev <d.kortelev@itransition.com>
2014-06-30 15:59:05 +0300

    fixes for IE8 and IE11

--- js/dms_view.js
+++ js/dms_view.js
@@ -22,8 +22,32 @@ var claIcon = function(fileName) {
 	return icon;
 };
 
-Modernizr.load([{
-	both: [
+// fix for indexOf in IE8
+if (!Array.prototype.indexOf)
+{
+	Array.prototype.indexOf = function(elt /*, from*/)
+	{
+		var len = this.length >>> 0;
+
+		var from = Number(arguments[1]) || 0;
+		from = (from < 0)
+			? Math.ceil(from)
+			: Math.floor(from);
+		if (from < 0)
+			from += len;
+
+		for (; from < len; from++)
+		{
+			if (from in this &&
+				this[from] === elt)
+				return from;
+		}
+		return -1;
+	};
+}
+
+yepnope([{
+	load: [
 		"/intranet/js/backbone-min.js",
 		"/intranet/dmsv/js/jquery.tmpl-min.js",
 
